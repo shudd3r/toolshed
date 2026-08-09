837745aa5f1737d135156d758447cdfa3821accc
shudd3r <q3.shudder@gmail.com>
2026-08-09 15:02:37 +0200

    Refactor tools tests

--- tests/ToolsTest.php
+++ tests/ToolsTest.php
@@ -19,75 +19,76 @@ use Shudd3r\Toolshed\Tests\Doubles\FakeProcessExecutor;
 
 class ToolsTest extends TestCase
 {
-    private static FakeProcessExecutor $processor;
-    private static VirtualDirectory    $toolsDir;
-
-    public static function setUpBeforeClass(): void
+    public function testInstallingKnownToolVersion()
     {
-        self::$processor = new FakeProcessExecutor();
-        self::$toolsDir  = VirtualDirectory::root('vfs://root');
+        $tools = $this->tools($processor, $toolsDir);
+        $id    = Tools\Identifier::fromStrings('test-tools/package', '12.6.2');
+        $processor->presetOutput(0, $id->packageName(), '12.6.2');
+
+        $installed = $tools->install($id);
+
+        $this->assertSame($id, $installed->identifier());
+        $toolDir = $toolsDir->subdirectory($id->installName());
+        $this->assertEquals($toolDir->subdirectory('vendor/bin'), $installed->binaries());
+        $this->assertTrue($toolDir->file('composer.json')->exists());
+        $this->assertSame([
+            (string) $toolDir => 'composer update --no-progress 2>&1'
+        ], $processor->commands);
     }
 
-    /** @dataProvider resolvedVersions */
-    public function testInstall_ReturnsResolvedTool(Tools\Identifier $id, string $outputVersion, string $resolvedVersion)
+    /** @dataProvider unresolvedVersions */
+    public function testInstallingRangeToolVersion(Tools\Identifier $id, string $outputVersion, string $resolveTo)
     {
-        self::$processor->presetOutput(0, $id->packageName(), $outputVersion);
-        $this->assertInstalled($id, $resolvedVersion, $this->tools()->install($id));
+        $tools = $this->tools($processor, $toolsDir);
+        $processor->presetOutput(0, $id->packageName(), $outputVersion);
+
+        $installed = $tools->install($id);
+
+        $expectedId = $id->resolvedTo(Tools\Identifier::parseConstraint($resolveTo));
+        $this->assertEquals($expectedId, $installed->identifier());
+
+        $resolveDir = $toolsDir->subdirectory($id->installName());
+        $this->assertFalse($resolveDir->exists());
+
+        $toolDir = $toolsDir->subdirectory($expectedId->installName());
+        $this->assertEquals($toolDir->subdirectory('vendor/bin'), $installed->binaries());
+        $this->assertTrue($toolDir->file('composer.json')->exists());
+        $this->assertSame([
+            (string) $resolveDir => 'composer update --dry-run --no-install --no-progress 2>&1',
+            (string) $toolDir    => 'composer update --no-progress 2>&1'
+        ], $processor->commands);
     }
 
     public function testErrorOnToolInstall_ThrowsException()
     {
-        self::$processor->presetOutput(2, 'package/name', '8.4.2');
+        $tools = $this->tools($processor, $toolsDir);
+        $processor->presetOutput(2, 'package/name', '8.4.2');
         $id = Tools\Identifier::fromStrings('test-tools/package', '12.6.2');
         $this->expectException(Tools\Exception\ToolSetupException::class);
-        $this->tools()->install($id);
+        $tools->install($id);
     }
 
     public function testOutputWithoutVersionPhrase_ThrowsException()
     {
-        self::$processor->presetOutput(0, 'different/name', '8.4.2');
+        $tools = $this->tools($processor, $toolsDir);
+        $processor->presetOutput(0, 'different/name', '8.4.2');
         $id = Tools\Identifier::fromStrings('test-tools/package', '^4.1');
         $this->expectException(Tools\Exception\ToolSetupException::class);
-        $this->tools()->install($id);
+        $tools->install($id);
     }
 
-    public static function resolvedVersions(): array
+    public static function unresolvedVersions(): array
     {
         return [
-            'exact version'       => [Tools\Identifier::fromStrings('test-tools/package', '12.6.2'), '12.6.2', '12.6.2'],
             'resolve to std'      => [Tools\Identifier::fromStrings('test-tools/package', '^3.0'), '3.5.0', '3.5.0'],
             'resolve to prefixed' => [Tools\Identifier::fromStrings('package/foo-bar', '^9.6'), 'v9.8.11', 'v9.8.11'],
             'resolve to branch'   => [Tools\Identifier::fromStrings('vendor/package', '@dev'), 'dev-master fbd47f7', 'dev-master']
         ];
     }
 
-    private function assertInstalled(Tools\Identifier $initial, string $version, Tools\Tool $installed): void
+    private function tools(?FakeProcessExecutor &$processor = null, ?VirtualDirectory &$toolsDir = null): Tools
     {
-        $commands = [];
-        if ($initial->isResolved()) {
-            $this->assertSame($expectedId = $initial, $installed->identifier());
-        } else {
-            $expectedId = $initial->resolvedTo(Tools\Identifier::parseConstraint($version));
-            $this->assertEquals($expectedId, $installed->identifier());
-
-            $resolveDir = self::$toolsDir->subdirectory('shared-tools/' . $initial->installName());
-            $this->assertFalse($resolveDir->exists());
-
-            $commands[(string) $resolveDir] = 'composer update --dry-run --no-install --no-progress 2>&1';
-        }
-
-        $expectedToolDir = self::$toolsDir->subdirectory('shared-tools/' . $expectedId->installName());
-        $this->assertEquals($expectedToolDir->subdirectory('vendor/bin'), $installed->binaries());
-        $this->assertTrue($expectedToolDir->file('composer.json')->exists());
-
-        $commands[(string) $expectedToolDir] = 'composer update --no-progress 2>&1';
-        $this->assertSame($commands, self::$processor->commands);
-    }
-
-    private function tools(): Tools
-    {
-        self::$toolsDir->subdirectory('shared-tools')->remove();
-        self::$processor->commands = [];
-        return new Tools(self::$processor, self::$toolsDir->subdirectory('shared-tools'));
+        $toolsDir ??= VirtualDirectory::root('vfs://root')->subdirectory('shared-tools');
+        return new Tools($processor ??= new FakeProcessExecutor(), $toolsDir);
     }
 }
