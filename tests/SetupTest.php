27735c0bc554d5a82f621885590267cdb3cd030f
shudd3r <q3.shudder@gmail.com>
2026-08-15 04:29:56 +0200

    Improve setup test assertions

--- tests/SetupTest.php
+++ tests/SetupTest.php
@@ -15,10 +15,14 @@ use PHPUnit\Framework\TestCase;
 use Shudd3r\Toolshed\Setup;
 use Shudd3r\Toolshed\SharedTools;
 use Shudd3r\Toolshed\RequestedTools;
+use Shudd3r\Toolshed\Filesystem;
+use Shudd3r\Toolshed\Tools;
+use Shudd3r\Toolshed\Sync;
 use Composer\Composer;
 use Composer\Package;
 use Composer\Config;
 use Composer\Semver;
+use Composer\Util;
 
 
 class SetupTest extends TestCase
@@ -32,7 +36,14 @@ class SetupTest extends TestCase
         $composer->setConfig($config = new Config());
         $config->merge(['config' => ['home' => $testDir->directory()]]);
 
-        $this->assertInstanceOf(SharedTools::class, $setup->sharedTools($composer, $io));
+        $sharedTools = $setup->sharedTools($composer, $io);
+
+        $toolsDir = Filesystem\Local\LocalDirectory::root($testDir->directory())->subdirectory('shared-tools');
+        $this->assertEquals(new SharedTools(
+            new Tools(new Util\ProcessExecutor(), $toolsDir),
+            new Sync\UsageRegistry(new Sync\RefData\LocalRefData($toolsDir->file('install-locations.json'))),
+            $io
+        ), $sharedTools);
     }
 
     public function testRequestedToolsBuilding()
@@ -44,7 +55,6 @@ class SetupTest extends TestCase
         $composer->setConfig($config = new Config());
         $composer->setPackage($package = new Package\RootPackage('client/project', '1.0.0', '1.0.0'));
         $config->merge(['config' => ['bin-dir' => $testDir->pathname('client/project/vendor/bin')]]);
-
         $package->setExtra(['shared-tools' => ['foo/bar', 'bar/baz', 'unlisted/package']]);
         $package->setRequires(['php' => $this->link('php', '6.0')]);
         $package->setDevRequires([
@@ -52,7 +62,13 @@ class SetupTest extends TestCase
             'bar/baz' => $this->link('bar/baz', '7.4.*')
         ]);
 
-        $this->assertInstanceOf(RequestedTools::class, $setup->requestedTools($composer, $io));
+        $requestedTools = $setup->requestedTools($composer, $io);
+
+        $clientDir = Filesystem\Local\LocalDirectory::root($testDir->pathname('client/project/vendor/bin'));
+        $this->assertEquals(new RequestedTools($clientDir, [
+            Tools\Identifier::fromStrings('foo/bar', '^12.4', '6.0'),
+            Tools\Identifier::fromStrings('bar/baz', '7.4.*', '6.0')
+        ]), $requestedTools);
         $error = '[SKIPPED] Shared dev tool `unlisted/package` not found in require-dev composer.json';
         $this->assertSame([$error], $io->errors);
     }
